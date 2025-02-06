954f97b19d2a3dfd540f71ea2a504e0e5f28c9d2
pathfindersh <43711751+pathfindersh@users.noreply.github.com>
2025-02-06 13:54:00 +0530

    3rd change

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,34 +2,28 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.30.0"
+      version = ">= 5.0.0, < 6.0.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5.1"
+      version = ">= 3.0.0, < 4.0.0"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.1.0"
+      version = ">= 4.0.0, < 5.0.0"
     }
 
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.4.0"
+      version = ">= 2.0.0, < 3.0.0"
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.23.0"
+      version = ">= 2.0.0, < 3.0.0"
     }
-  }
-
-  backend "s3" {
-    bucket = "vprofile-github-action"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
   }
 
   required_version = ">= 1.6.3, < 1.11"
